bbc4b1c005ff8eab5e0a8b19f0aaa4afc8b38ce8
Laura Royle <lauraamie.royle@gmail.com>
2019-02-13 11:20:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,7 @@ end
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |key, value|
     value << supply
-
+  end
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
